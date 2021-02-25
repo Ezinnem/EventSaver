2f0bc5099ede61c1521f3d01bfd216455e1e8bd4
Ezinne <nnamani.ezinne@gmail.com>
2021-02-25 10:14:27 +0100

    Implemented the server part of the user login using JWT

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -48,11 +48,21 @@ export const deletePost = async(req, res) => {
 export const likePost = async (req, res) => {
     const { id } = req.params;
     
+    if(!req.userId) return res.json({ message: "Unauthorized"});
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("Invalid post Id");
 
     const post = await PostMessage.findById(id)
     
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true});
+    const index = post.likes.findIndex((id) => id === String(req.userId) );
+
+    if(index === -1 ) {
+        //like the post
+        post.likes.push(req.userId);
+    }else {
+        //dislike the post
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true});
 
     res.json(updatedPost);
 }
--- server/index.js
+++ server/index.js
@@ -5,19 +5,16 @@ import dotenv from 'dotenv'
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
-
+import userRoutes from './routes/users.js';
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json( { limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ linit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
-app.use('/', (req, res) => {
-    res.send("Hello to Event saver")
-})
+app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
